7605a654e5d66f296771593d8fbd77cc2ab31b36
MelguiXD <guiganeves@hotmail.com>
2023-11-03 17:38:27 -0300

    Adding Datadog

--- lib/latam_logger/log/logging.rb
+++ lib/latam_logger/log/logging.rb
@@ -2,24 +2,25 @@
 
 require "logger"
 require "json"
-#require "ddtrace"
 
 module LatamLogger
   module Log
     class Logging
       attr_reader :logger
-      #attr_reader :order_id
 
-      def initialize #(order_id)
-        #@order_id = order_id
+      def initialize
         @logger = Logger.new(ENV['LATAM_LOGGER_PATH_FILE'] || STDOUT)
         @logger.formatter = method(:format_log)
+        Datadog::Tracing.trace('operations') {
+          @correlation = Datadog::Tracing.correlation
+        }
       end
 
       %w[debug info warn error].each do |level|
         define_method(level) do |log|
-          #Datadog::Tracing.trace('@order_id') {@logger.send(level, log)}
-          @logger.send(level, log)
+          Datadog::Tracing.trace('operations') {
+            @logger.send(level, log)
+          }
         end
       end
 
@@ -27,6 +28,7 @@ module LatamLogger
         if msg.is_a?(Hash)
           msg[:level] = severity
           msg[:date] = datetime
+          msg[:"dd.trace_id"] = @correlation.trace_id
         else
           msg = {
             level: severity,
